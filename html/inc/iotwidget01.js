b266bd4381e1507c5b07096732748925800bddb9
Marco Sillano <msillano@users.noreply.github.com>
2024-10-04 11:26:57 +0200

    Add files via upload

--- html/inc/iotwidget01.js
+++ html/inc/iotwidget01.js
@@ -6,15 +6,23 @@ License MIT
 (C)2024 marco.sillano@gmail.com
 version 1.0 09/09/2024
  */
+ /*  add to  main (see example clima01.html)
+ <script type="text/javascript" src="inc/restget.js"> </script >
+ // REQUIRED by iotwidget01
+  <script type="text/javascript" src="inc/iotwidget01.js"> </script >
+     <!-- REQUIRED Stylesheets by iotwidget01 	-->
+<link rel = "stylesheet" href ="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+<link rel = "stylesheet" href ="inc/iotwidget01.css" >
+*/
 
 function getDeviceIcon(data, tooltip = false){
   let tmp  =  "<span class='hover'><span class='fa fa-2x' style='color:"+ data.icon.color + "; vertical-align: middle;'>";
   tmp +=  String.fromCharCode(data.icon.code)+"</span>";
   if (tooltip)
      tmp +=  "<span class='tooltip'><b>"+data.name+"</b><br>"+decodeURI(data.tooltip)+"</span></span>";
-  else 
+  else
      tmp +=  "<span class='tooltip'><b>"+data.name+"</b></span></span>";
-return (tmp);
+  return (tmp);
 }
 
 function getButton(data, image = false){
@@ -23,14 +31,14 @@ function getButton(data, image = false){
 	      tmp =  "<button class='imgbutton' style='background-image: url(\"";
  	      tmp += data.image+"\");' onclick='RESTget(\""+baseURL + "execute/" + data.scenerule +"\")' ><span ";
  	      tmp += (data.color? "style ='color : "+data.color+";' ":" ")+ ">"+data.text+"</span></button>";
-	} else {
+  	} else {
           tmp =  "<input id='button"+ data.id +"' type='button' class='bigbutton' ";
           tmp += "style ='"+(data.color? " color : "+data.color+"; ":"");
           tmp += (data.background? " background-color : "+data.background+"; ":" ") + "' value='"+data.text+"'";
           tmp += " onclick='RESTget(\""+baseURL + "execute/" + data.scenerule +"\" )' />";
     }
-    return(tmp); 	
-}	
+  return(tmp);
+}
 
 
 
@@ -43,8 +51,8 @@ function doIotwidget01(item) {
                         const outputDiv = document.getElementById('item'+ item.id);
                         outputDiv.innerHTML = "<span  class='value' "+(item.color? "style ='color : "+item.color+";' ":"")+ "><b>"+ (data[item.code] || data['error'])+"</b></span>";
  				        });
- 				break; 
- 				
+ 				break;
+
  		   case "bigvalue":
 		        RESTget(baseURL + "device/" + item.devId +"/" + item.code)
 			        .then(data => {
@@ -52,17 +60,17 @@ function doIotwidget01(item) {
                      const outputDiv = document.getElementById('item'+ item.id);
                         outputDiv.innerHTML = "<span  class='bigvalue' "+(item.color? "style ='color : "+item.color+";' ":"")+ "><b>"+(data[item.code] || data['error'])+"</b></span>";
  				        });
- 				break; 
-				
- 	       case "icon":
+ 				break;
+
+ 	      case "icon":
 		        RESTget(baseURL + "device/" + item.devId +"/dstatus")
 			        .then(data => {
          			 //       document.getElementById('output').innerText += JSON.stringify(data);  // only for test
                        const outputDiv = document.getElementById('item'+ item.id);
                         outputDiv.innerHTML = getDeviceIcon(data);
  				        });
- 				break; 
- 				
+ 			 break;
+
  		   case "icotip":
 		        RESTget(baseURL + "device/" + item.devId +"/dstatus")
 			        .then(data => {
@@ -70,23 +78,21 @@ function doIotwidget01(item) {
    	                const outputDiv = document.getElementById('item'+ item.id);
                         outputDiv.innerHTML = getDeviceIcon(data, true);
  				        });
- 				break; 
- 				
+ 			 break;
+
  		   case "bigbutton":
 	            let outputDiv1 = document.getElementById('item'+ item.id);
 	            outputDiv1.innerHTML = getButton(item);
- 				break; 
- 	
+ 				break;
+
 		   case "imgbutton":
 	            let outputDiv2 = document.getElementById('item'+ item.id);
  	            outputDiv2.innerHTML = getButton(item, true);
- 				break; 
- 				
-           default:
-               if (doIotwidget02) doIotwidget02 (item);  // extension
-		}    		
-	
-	
-} 
+ 				break;
+ 	// to auto-extend widget library
+        default:
+               if (typeof(window.doIotwidget02) === "function") {doIotwidget02 (item);}  // extension
+		}
 
 
+}
