d3da44e8e518e053272893dd351bcecd40df4c6f
Marco Sillano <msillano@users.noreply.github.com>
2025-01-23 19:12:21 +0100

    Add files via upload

--- addon/cloner01.js
+++ addon/cloner01.js
@@ -13,6 +13,8 @@ per IOTwebUI version 2.2 10/08/2024
 // want to take advantage of the more complete "Query Properties" API, which can show more properties.
 // Example: see document https://github.com/msillano/tuyaDAEMON/blob/main/devices/TY-08Z/TY_08Z_01.pdf
 // The code is executed at any polling, to update device property values.
+// NOTE: this is resources consuming device! So it is set in 'idle' status using 'standby' input (e.g. from applications)
+// At startup it makes a device refresh, then sets standby to true 
 
 // =====================  USE AS NEW MACRO
 // 0) This file (updated) must be in the 'addons' directory of your IoTwebUI installation
@@ -32,13 +34,7 @@ per IOTwebUI version 2.2 10/08/2024
 // You can use 'CLONER01( dev_id, xname [, room, home])' as new MACRO in RULE-pad.
 // note: [, room, home] are optional, because default are defined in code. You can change the defaults.  
 
-// =====================  USE AS RULE (no MACRO)
-// 1) Use the 'minified' version, as RULE (UPDATE the code if required!)
-// 1-A) Copy the 'minified CLONER01' + 'RULES for CLONER01' (excluding MACRO call) in the RULE-pad at run time (temporary)
-// 2-B) Copy the 'minified CLONER01' + 'RULES for CLONER01' (excluding MACRO call) in the 'var usrrules' in the usrrulesXX.X.js file (permanent).
-// 3) optional: update 'custom.js' to set icon and color for this x-devices
-
-// ==== end use instructionsSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+// ==== end use instructions
 
 // =================== CLONER01 CODE
 
@@ -47,27 +43,65 @@ per IOTwebUI version 2.2 10/08/2024
 function CLONER01( dev_id, xname, room = "Test", home = 'ADMIN') {             // default params
     
 // ====== Use callAPI() to get fresh data
- 	let _clStatus =[];
-    if ( GETATTRIBUTE(dev_id, 'online', false)){
-       const api_url = "/v2.0/cloud/thing/"+dev_id+"/shadow/properties";
-       const x = callAPI('GET', api_url); 
-		   console.log(x);  // for debug only
-	// ====== Format data for the device 'status' as [{code, value},...]
-	   if (x) {
-		   x.properties.forEach( (p) => {   
-					 let item ={
-						 code : p.code,
-						 value: p.value
-					 };
-					 _clStatus.push( item); 
-					 });		 
-		//   console.log(_clStatus);   // for debug only
+ 	let _clStatus =[{
+						 code : "standby",
+						 value: true
+					 }
+	];
+	let first = !GETATTRIBUTE(xname, 'name', false);         // exists ?
+	if (first){
+	ADDXDEVICE(home, room, xname, _clStatus );
+	}
+	let device = getDeviceFromRef(xname) || null;
+	if (first){
+             device["clone"] = true;
+             device["fromdevice"] = dev_id;
+             device['details'] = [
+            {
+                from: {
+                    type: "extra",
+                    id: "Tuya\\nCloud"
+                },
+                action: "get schema",
+            }, {
+                action: "<I>loop</I>",
+            }, {
+                from: {
+                    type: "xtap",
+                    id: "CLONER ON"
+                },
+                action: "standby=false",
+            },  {
+                from: {
+                    type: "xtap",
+                    id: "CLONER OFF"
+                },
+                action: "standby=true",
+			} ]; 
+         }
+ 	let tuyaOK = GETATTRIBUTE(dev_id, 'online', false);
+	let isIdle = !device || device.status.filter((attr) => attr.code == "standby")[0].value;
+	if (first && !tuyaOK){
+		 myMsgBox("CLONER01","WARNING: The Tuya device "+dev_id+" is offline.");
+	}
+//	 console.log("CLONER-1: isIdle:"+isIdle+" first:"+first);
+    if (!first && tuyaOK){   //  device tuya online and not first ?
+		 let asData = device.status.length > 1;
+//		 console.log("CLONER-2 isIdle:"+isIdle+" asData:"+asData+" first:"+first);
+		 if (!isIdle || !asData) {        // not idle OR no data
+			   const api_url = "/v2.0/cloud/thing/"+dev_id+"/shadow/properties";
+			   const x = callAPI('GET', api_url); 
+//			   console.log(x);  // for debug only
+		// ====== Format data for the device 'status' as [{code, value},...]
+			   if (x) {
+				   x.properties.forEach( (p) => {   
+   						     SETXDEVICESTATUS(xname, p.code, p.value);
+							 });		 
+			   }
+		 }
+	}
 	// ====== Create/refresh the x-device
-		   ADDXDEVICE(home, room, xname, _clStatus );
-	       }
-	     }
-	   SETXDEVICEONLINE(xname, GETATTRIBUTE(dev_id, 'online', false));  // set it online
-  
+ SETXDEVICEONLINE(xname, GETATTRIBUTE(dev_id, 'online', false) && !isIdle );  // set it online
 }
 // end  CLONER01 code
 
@@ -80,15 +114,3 @@ function CLONER01( dev_id, xname, room = "Test", home = 'ADMIN') {             /
   CLONER01("df123456xwyz876543xxww", "xtest");                //  MACRO call
 */
 
-/*
-// =============================  minified CLONER01
-//   Minified version of CLONER01 for RULE-pad: 2 lines only!  (using Notepad++ + plugin JSTool).
-//   On the RULE-pad you can cut long lines in many rows, and use 'continue' char (\) as last char for any row.
-// You MUST update values to meet your structure in the next row:
-
-var _cldev_id='xxxyyyzzz',_clxname='xtest',_clroom="Test",_clhome='ADMIN';const api_url="/v2.0/cloud/thing/"+_cldev_id+"/shadow/properties";var  _clx=callAPI('GET',api_url);
-
-let _clStatus=[];_clx.properties.forEach((p)=>{let item={code:p.code,value:p.value};_clStatus.push(item);}); ADDXDEVICE(_clhome,_clroom,_clxname,_clStatus);SETXDEVICEONLINE(_clxname,true);
-
-
-*/
